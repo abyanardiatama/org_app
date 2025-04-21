01b7f692ca0c4578af8aa5e67882089901577e81
abyanardiatama <abyanardiatama@students.undip.ac.id>
2025-04-21 12:18:42 +0700

    feat: add logo_kmi image and update logo display in admin views

--- resources/views/filament/admin/logo.blade.php
+++ resources/views/filament/admin/logo.blade.php
@@ -1,12 +1,22 @@
 @if (request()->routeIs('filament.admin.auth.login'))
     {{-- Logo untuk halaman login --}}
-    <div class="flex flex-col items-center">
-        <img src="{{ asset('logo_upn.png') }}" class="h-10" alt="Logo Login">
+    <div class="flex items-center gap-x-4">
+        <img src="{{ asset('logo_upn.png') }}" class="h-10 p-5" alt="Logo Login">
+        <img src="{{ asset('logo_kmi.png') }}" class="h-10 p-5" alt="Logo Login">
     </div>
 @else
     {{-- Logo untuk dashboard --}}
-    <div class="flex items-center space-x-3">
-        <img src="{{ asset('logo_upn.png') }}" class="h-8" alt="Logo Dashboard">
+    {{-- <div class="flex items-center space-x-3">
+        <img src="{{ asset('logo_upn.png') }}" class="h-8 pr-4" alt="Logo Dashboard">
+        <img src="{{ asset('logo_kmi.png') }}" class="h-10 pl-5" alt="Logo Login">
         <span class="text-sm font-semibold text-gray-800 p-4">Sistem Informasi <br>Management KMI</span>
+    </div> --}}
+    <div class="flex items-center gap-x-3">
+        <img src="{{ asset('logo_upn.png') }}" class="h-8" alt="Logo Dashboard">
+        <img src="{{ asset('logo_kmi.png') }}" class="h-10" alt="Logo Login">
+        <span class="text-sm font-semibold text-gray-800">
+            Sistem Informasi <br>Management KMI
+        </span>
     </div>
+    
 @endif
